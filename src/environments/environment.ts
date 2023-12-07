456db9fbed5db3453b10e2c2f87f6b7d7e246fce
marcelaGonzalezArancibia <marce.gonzalez@duocuc.cl>
2023-12-07 19:08:06 -0300

    segundo commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBHTrC6A9JNWgaq65VOkiJy3LIDCqtnUp8",
+    authDomain: "proyecto-movil-80f7f.firebaseapp.com",
+    projectId: "proyecto-movil-80f7f",
+    storageBucket: "proyecto-movil-80f7f.appspot.com",
+    messagingSenderId: "224792029073",
+    appId: "1:224792029073:web:9121334c9bca8519951c30"
+  }
 };
 
 /*
